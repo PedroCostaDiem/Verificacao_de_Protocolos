aaaec457f392b4657bd8db320cd974eb85e2e81f
PedroCostaDiem <123518716+PedroCostaDiem@users.noreply.github.com>
2024-08-20 10:51:00 -0300

    Delete src directory

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import { Love } from './Love';
-import { Hate } from './Hate';
-import { Happy } from './Happy';
-import { Fear } from './Fear';
-import { CryingFace } from './CryingFace';
-
-export { Love, Hate, Happy, CryingFace, Fear };
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import { Love, Hate, Happy, CryingFace, Fear } from './components';
-
-export { Love, Hate, Happy, CryingFace, Fear };
